c414c65a92d7f2ad5da2872fa66cd2192c873850
lisapatrikeevna <lisa15.08patrikeevna@gmail.com>
2021-01-31 18:12:06 +0200

    Updates

---
+++ precache-manifest.464a8112de80ca0038e34a97bbb3bb66.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b28a4c228ec21b4bf2487118922ea140",
+    "url": "/sotialTS/index.html"
+  },
+  {
+    "revision": "04b733b9b9643804778a",
+    "url": "/sotialTS/static/css/main.8f1833dd.chunk.css"
+  },
+  {
+    "revision": "94253fc866e98c862bba",
+    "url": "/sotialTS/static/js/2.7765844a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/sotialTS/static/js/2.7765844a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04b733b9b9643804778a",
+    "url": "/sotialTS/static/js/main.2dfcc5e5.chunk.js"
+  },
+  {
+    "revision": "94a76d3cbe32f94b90f7",
+    "url": "/sotialTS/static/js/runtime-main.34a6b334.js"
+  },
+  {
+    "revision": "58f7567628ee9e548efa45d9b86df372",
+    "url": "/sotialTS/static/media/806.58f75676.gif"
+  },
+  {
+    "revision": "60a046700725c6bda6e94bfa0a2424c2",
+    "url": "/sotialTS/static/media/baner1.60a04670.png"
+  },
+  {
+    "revision": "fa1cca2c39683f9c402e3d85e17454aa",
+    "url": "/sotialTS/static/media/bg1.fa1cca2c.jpeg"
+  },
+  {
+    "revision": "f5155bffd7db2f69c3be842a547bb4a7",
+    "url": "/sotialTS/static/media/bg2.f5155bff.jpg"
+  },
+  {
+    "revision": "85a01a6e97a6461fea584577c45e18eb",
+    "url": "/sotialTS/static/media/bg3.85a01a6e.jpg"
+  },
+  {
+    "revision": "e60b3bce1575da2ef212aeec441f4311",
+    "url": "/sotialTS/static/media/bg4.e60b3bce.jpg"
+  },
+  {
+    "revision": "0e8d99a035837cb65b64985ea63f6211",
+    "url": "/sotialTS/static/media/blue.0e8d99a0.jpg"
+  },
+  {
+    "revision": "2eabbef1c42365b022fbbe0fea0eb67f",
+    "url": "/sotialTS/static/media/dark.2eabbef1.jpg"
+  },
+  {
+    "revision": "75cb4506decc2b410f839f2a96a681c1",
+    "url": "/sotialTS/static/media/light.75cb4506.jpg"
+  },
+  {
+    "revision": "73b874f6d8289f87dfd720f7ff99f262",
+    "url": "/sotialTS/static/media/maskFor1.73b874f6.png"
+  },
+  {
+    "revision": "df029ff1d758f81d63da9f04611a8c69",
+    "url": "/sotialTS/static/media/red.df029ff1.jpg"
+  },
+  {
+    "revision": "251941560ca5371eb1ff919468a55504",
+    "url": "/sotialTS/static/media/search.25194156.png"
+  },
+  {
+    "revision": "defd5d0eed71cbfb8f1a963295aab2f9",
+    "url": "/sotialTS/static/media/undraw_cloud_docs_p4ob.defd5d0e.png"
+  },
+  {
+    "revision": "ce1bbad0641ef7fe5460fb224cf8d44f",
+    "url": "/sotialTS/static/media/undraw_data_processing_yrrv.ce1bbad0.png"
+  },
+  {
+    "revision": "05f7699010385b3e2ebb4676a2f5f0fc",
+    "url": "/sotialTS/static/media/undraw_design_team_af2y.05f76990.png"
+  },
+  {
+    "revision": "855d21abe0e22a0a0062dcd5a1342773",
+    "url": "/sotialTS/static/media/undraw_experience_design_eq3j.855d21ab.png"
+  },
+  {
+    "revision": "11abe06ac98fa7a7c31110a43d9d0f44",
+    "url": "/sotialTS/static/media/undraw_good_team_m7uu.11abe06a.png"
+  },
+  {
+    "revision": "c7b1f90fb314bf9429ba9ac9682a07dc",
+    "url": "/sotialTS/static/media/undraw_mobile_web_2g8b.c7b1f90f.png"
+  },
+  {
+    "revision": "3522b75db0f48fc75c390e8e146bdf5e",
+    "url": "/sotialTS/static/media/undraw_online_popularity_elhc.3522b75d.png"
+  },
+  {
+    "revision": "24b9c595a9c86d0facf9c9bc0dc70cc2",
+    "url": "/sotialTS/static/media/undraw_online_test_gba7.24b9c595.png"
+  },
+  {
+    "revision": "c2e88523b652faaf859386621a39172f",
+    "url": "/sotialTS/static/media/undraw_reminder_pa79.c2e88523.png"
+  },
+  {
+    "revision": "e2f70a4de36ff22d4cf7df3c0d08a67f",
+    "url": "/sotialTS/static/media/undraw_video_upload_3d4u.e2f70a4d.png"
+  }
+]);
